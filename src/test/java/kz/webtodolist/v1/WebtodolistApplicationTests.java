ee5cea2e9f036c4620f9ee047460276a41ff90e9
Aleksandr-code <Aleksandr.chess@mail.ru>
2020-09-24 12:52:37 +0600

    Initial commit

---
+++ src/test/java/kz/webtodolist/v1/WebtodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.webtodolist.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
